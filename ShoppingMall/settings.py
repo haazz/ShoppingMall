831627a8fabb4c4dde718a597edfbb63ec31d4b5
haazz <gkwldnjs121@gmail.com>
2023-04-01 15:35:45 +0900

    add main page and app url

--- ShoppingMall/settings.py
+++ ShoppingMall/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'user',
+    'product',
+    'order',
+    'main_page',
 ]
 
 MIDDLEWARE = [
