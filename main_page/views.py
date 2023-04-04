7f58cc9513bb979767237a87e28c5eb546ee7639
haazz <gkwldnjs121@gmail.com>
2023-04-04 09:19:35 +0900

    add navbar

--- main_page/views.py
+++ main_page/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def main(request):
-    return render(request, 'main_page/main.html')
+def main_page(request):
+    return render(request, 'main_page/main_page.html')
