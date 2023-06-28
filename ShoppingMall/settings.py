2b558526de328681c18cefdbc627284106c537a2
haazz <gkwldnjs121@gmail.com>
2023-06-28 23:05:46 +0900

    PostgreSQL setting on Docker

--- ShoppingMall/settings.py
+++ ShoppingMall/settings.py
@@ -25,10 +25,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-=an9!pgwa!*&d+i$h)(y!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', 1))
 
-# if os.environ.get('DJANGO_ALLOWED_HOSTS'):
-#     ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
-# else:
-ALLOWED_HOSTS = ['*']
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+     ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = [ ]
 
 
 # Application definition
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'ShoppingMall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("SQL_ENGINE", 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get("SQL_PORT", '5432'),
     }
 }
 
